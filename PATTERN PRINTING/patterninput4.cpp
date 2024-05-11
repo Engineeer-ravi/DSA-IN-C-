08862acec2be2686ca031cccf71053f7ef1b8e09
Engineeer-ravi <pathakravi.er@gmail.com>
2024-05-12 00:01:32 +0530

    work

--- PATTERN PRINTING/patterninput4.cpp
+++ PATTERN PRINTING/patterninput4.cpp
@@ -2,10 +2,8 @@
 using namespace std;
 int main()
 {
-    int n,r,c;
-    cout<<"Enter the number: ";
-    cin>>n;
-    for(int r=1; r<=n; r++)
+    int r,c;
+    for(int r=4; r>=1; r--)
     {
         for(int c=1; c<=r; c++)
         {
