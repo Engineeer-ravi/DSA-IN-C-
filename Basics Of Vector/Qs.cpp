780870b42f604bf06420bf935dcabe6f83297fe7
Engineeer-ravi <pathakravi.er@gmail.com>
2024-11-05 23:31:57 +0530

    ode

--- Basics Of Vector/Qs.cpp
+++ Basics Of Vector/Qs.cpp
@@ -1,3 +1,23 @@
+// #include<iostream>
+// #include<vector>
+// using namespace std;
+// int main(){
+
+//        vector<int> v;
+//        v.push_back(2);
+//          v.push_back(9);
+//            v.push_back(7);
+//              v.push_back(5);
+//                 v.push_back(8);
+//                   v.push_back(3);
+//                   for(int i=0; i<v.size(); i++){
+//                     cout<<v[i]<<" ";
+
+//                   }
+
+
+
+// }
 #include<iostream>
 #include<vector>
 using namespace std;
