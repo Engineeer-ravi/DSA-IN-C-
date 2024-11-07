5bf563e69681df236f255a72c568adab72c85a3e
Engineeer-ravi <pathakravi.er@gmail.com>
2024-11-07 23:29:00 +0530

    c++ Code

--- Basics Of Vector/tekingInput.cpp
+++ Basics Of Vector/tekingInput.cpp
@@ -1,3 +1,21 @@
+// #include<iostream>
+// #include<vector>
+// using namespace std;
+// int main(){
+//     vector<int> v;
+//     for(int i=0; i<5; i++){
+//         int  x;
+//         cin>>x;
+//         v.push_back(x);
+//     }
+
+//     for (int i=0; i<5; i++){
+//         cout<<v[i];
+//     }
+
+// }
+
+
 #include<iostream>
 #include<vector>
 using namespace std;
